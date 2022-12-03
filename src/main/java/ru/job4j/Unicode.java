1417a338e03e72c2f09c607488e4850edce7180b
Trush22 <89226079875@mail.ru>
2022-12-03 14:32:02 +0500

    9.4. Символы в Java и их особенности [#504920 #367008]

---
+++ src/main/java/ru/job4j/Unicode.java
@@ -0,0 +1,16 @@
+package ru.job4j;
+
+public class Unicode {
+    public static void main(String[] args) {
+        char c1 = '\u0035';
+        char c2 = '\u004D';
+        char c3 = '\u006E';
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+        System.out.println("\u0031\u0032\u0033");
+        char a = 'A';
+        char b = 'B';
+        System.out.println(a + b);
+    }
+}
---
+++ src/main/java/ru/job4j/Unicode2.java
@@ -0,0 +1,15 @@
+package ru.job4j;
+
+public class Unicode2 {
+    public static void main(String[] args) {
+        int a = 'A';
+        int b = 'B';
+        System.out.println("A: " + a);
+        System.out.println("B: " + b);
+        System.out.println("A + B: " + (a + b));
+        char c = 'C';
+        System.out.println(++c);
+        char d = 'D';
+        System.out.println(--d);
+    }
+}
