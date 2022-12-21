71f84a6048f54daddf7f2fa8f272be8ad5b78043
Trush22 <89226079875@mail.ru>
2022-12-21 23:24:48 +0500

    5.7. Ипотека [#156315 #373129]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -2,11 +2,10 @@ package ru.job4j.loop;
 
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
-        int year = 1;
-        double sum = amount * percent / 100 + amount;
-        while (salary < sum) {
-            sum = sum - salary;
-            year = year + 1;
+        int year = 0;
+        while (amount > 0) {
+            amount += amount * percent / 100 - salary;
+            year++;
         }
         return year;
     }
